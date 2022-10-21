4a4307c7fc652cf8311c1c276d0ac4e694347a8e
chblodg <shotah@users.noreply.github.com>
2022-10-21 16:37:22 -0700

    adding qty check (#3)
    Co-authored-by: christopher blodgett <christopher.blodgett@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -72,3 +72,20 @@ def test_parkinglot_get_space_occupied():
     space_large = test_parkinglot.get_space(size="large")
     assert test_parkinglot.reserve_space(spot=space_large)
     assert test_parkinglot.get_space(spot=space_large)["occupied"]
+
+
+def test_parkinglot_get_amount_of_available_spaces():
+    test_parkinglot = parkinglot()
+    test_parkinglot.set_floors(floors=2)
+    test_parkinglot.set_floor_spaces(floor=0, small=2, medium=2, large=2)
+    test_parkinglot.set_floor_spaces(floor=1, medium=1, large=1)
+    space_large = test_parkinglot.get_space(size="large")
+    available_spaces_before_reservation = (
+        test_parkinglot.get_amount_of_available_spaces(size="large")
+    )
+    test_parkinglot.reserve_space(spot=space_large)
+    available_spaces_after_reservation = test_parkinglot.get_amount_of_available_spaces(
+        size="large"
+    )
+    assert available_spaces_before_reservation == 3
+    assert available_spaces_after_reservation == 2
