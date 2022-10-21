ec7030d4b09abac06ab141b67d9352151bff20a0
christopher blodgett <christopher.blodgett@gmail.com>
2022-10-21 16:23:44 -0700

    adding the rest of my files

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from structlog import get_logger
+
+logger = get_logger(__name__)
+
 
 class automobile:
     wheels = 4
@@ -13,5 +17,60 @@ class car(automobile):
         self.size = "small"
 
 
-def main():
-    return car()
+class parkinglot:
+    def __init__(self) -> None:
+        self.garage = {}
+
+    def get_space(self, spot: dict = {}, size: str = "small") -> dict:
+        if not spot:
+            return self.get_empty_space(size=size)
+        return self.garage[spot["floor"]][spot["spot"]]
+
+    def __check_space_is_empty(self, *, floor: int, space: dict, size: str) -> bool:
+        if not size:
+            return self.garage[floor][space]["occupied"] is False
+        return (
+            self.garage[floor][space]["occupied"] is False
+            and self.garage[floor][space]["size"] == size
+        )
+
+    def get_empty_space(self, *, size: str) -> dict:
+        for floor in self.garage.keys():
+            for space in self.garage[floor]:
+                if self.__check_space_is_empty(floor=floor, space=space, size=size):
+                    return self.garage[floor][space]
+        return {}
+
+    def reserve_space(self, spot: dict) -> bool:
+        if not spot:
+            return False
+        self.garage[spot["floor"]][spot["spot"]]["occupied"] = True
+        return self.garage[spot["floor"]][spot["spot"]]["occupied"]
+
+    def set_floors(self, *, floors: int) -> None:
+        if not floors:
+            self.garage = {0: {}}
+        self.garage = {floor: {} for floor in range(floors)}
+
+    def __set_floor_space(
+        self, spot_num: int = 0, floor: int = 0, size: dict = {}
+    ) -> int:
+        if not size:
+            return spot_num
+        for size, qty in size.items():
+            for _ in range(qty):
+                self.garage[floor][spot_num] = {
+                    "spot": spot_num,
+                    "occupied": False,
+                    "size": size,
+                    "floor": floor,
+                }
+                spot_num += 1
+        return spot_num
+
+    def set_floor_spaces(
+        self, *, floor: int = 0, small: int = 0, medium: int = 0, large: int = 0
+    ) -> None:
+        spot_num = 0
+        for size in [{"small": small}, {"medium": medium}, {"large": large}]:
+            spot_num = self.__set_floor_space(spot_num=spot_num, floor=floor, size=size)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,74 @@
 from __future__ import annotations
 
-from ..main import car
+from structlog import get_logger
+
+from ..main import car, parkinglot
+
+logger = get_logger(__name__)
 
 
 def test_car():
     test_car = car()
     assert test_car.wheels == 4
     assert test_car.size == "small"
+
+
+def test_parkinglot_set_floors():
+    test_parkinglot = parkinglot()
+    assert test_parkinglot.garage == {}
+    test_parkinglot.set_floors(floors=4)
+    assert test_parkinglot.garage[0] == {}
+    assert test_parkinglot.garage == {0: {}, 1: {}, 2: {}, 3: {}}
+
+
+def test_parkinglot_set_floor_spaces():
+    test_parkinglot = parkinglot()
+    test_parkinglot.set_floors(floors=2)
+    test_parkinglot.set_floor_spaces(floor=0, small=2, medium=2, large=2)
+    test_parkinglot.set_floor_spaces(floor=1, medium=1, large=1)
+    assert test_parkinglot.garage[1][1] == {
+        "spot": 1,
+        "floor": 1,
+        "occupied": False,
+        "size": "large",
+    }
+
+
+def test_parkinglot_get_space():
+    test_parkinglot = parkinglot()
+    test_parkinglot.set_floors(floors=2)
+    test_parkinglot.set_floor_spaces(floor=0, small=2, medium=2, large=2)
+    test_parkinglot.set_floor_spaces(floor=1, medium=1, large=1)
+    available_space = test_parkinglot.get_space()
+    assert available_space == {
+        "spot": 0,
+        "floor": 0,
+        "occupied": False,
+        "size": "small",
+    }
+    available_space_large = test_parkinglot.get_space(size="large")
+    assert available_space_large == {
+        "spot": 4,
+        "floor": 0,
+        "occupied": False,
+        "size": "large",
+    }
+
+
+def test_parkinglot_reserve_space():
+    test_parkinglot = parkinglot()
+    test_parkinglot.set_floors(floors=2)
+    test_parkinglot.set_floor_spaces(floor=0, small=2, medium=2, large=2)
+    test_parkinglot.set_floor_spaces(floor=1, medium=1, large=1)
+    available_space_large = test_parkinglot.get_space(size="large")
+    assert test_parkinglot.reserve_space(spot=available_space_large)
+
+
+def test_parkinglot_get_space_occupied():
+    test_parkinglot = parkinglot()
+    test_parkinglot.set_floors(floors=2)
+    test_parkinglot.set_floor_spaces(floor=0, small=2, medium=2, large=2)
+    test_parkinglot.set_floor_spaces(floor=1, medium=1, large=1)
+    space_large = test_parkinglot.get_space(size="large")
+    assert test_parkinglot.reserve_space(spot=space_large)
+    assert test_parkinglot.get_space(spot=space_large)["occupied"]
