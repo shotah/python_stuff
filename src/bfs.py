9c6de5ea06b1e6f276770b5348302a5015b2b6c5
chblodg <shotah@users.noreply.github.com>
2022-10-25 12:23:41 -0700

    adding workflow items for github (#7)
    Co-authored-by: christopher blodgett <christopher.blodgett@gmail.com>

--- src/bfs.py
+++ src/bfs.py
@@ -10,40 +10,41 @@ class TreeNode:
     left = None
     right = None
 
-    def __init__(self, *, value: int):
+    def __init__(self, *, value: int) -> None:
         self.value = value
         self.left: Union[TreeNode, None] = None
         self.right: Union[TreeNode, None] = None
 
 
 class Tree:
-    def __init__(self, *, value: int):
+    def __init__(self, *, value: int) -> None:
         self.root: Union[TreeNode, None] = TreeNode(value=value)
 
-    def __get_prev_node(self, *, root_node: Union[TreeNode, None], value: int):
+    def __get_prev_node(
+        self, *, root_node: Union[TreeNode, None], value: int
+    ) -> Union[TreeNode, None]:
         prev_node: Union[TreeNode, None] = None
         curr_node = root_node
         while curr_node:
+            prev_node: Union[TreeNode, None] = curr_node
             if curr_node.value > value:
-                prev_node: Union[TreeNode, None] = curr_node
                 curr_node: Union[TreeNode, None] = curr_node.left
             else:
-                prev_node: Union[TreeNode, None] = curr_node
                 curr_node: Union[TreeNode, None] = curr_node.right
         return prev_node
 
-    def __set_prev_node(self, *, prev_node: Union[TreeNode, None], value: int):
+    def __set_prev_node(self, *, prev_node: Union[TreeNode, None], value: int) -> None:
         if prev_node:
             if prev_node.value > value:
                 prev_node.left = TreeNode(value=value)
             else:
                 prev_node.right = TreeNode(value=value)
 
-    def insert(self, *, value: int):
+    def insert(self, *, value: int) -> None:
         prev_node = self.__get_prev_node(root_node=self.root, value=value)
         self.__set_prev_node(prev_node=prev_node, value=value)
 
-    def in_order(self):
+    def in_order(self) -> list:
         stack = []
         response = []
         while self.root or len(stack):
